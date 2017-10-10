5154951339a982d2397d02a8990047ebc0c35732
Blake Huber <blakeca00@gmail.com>
2017-10-09 20:25:51 -0500

    install_testpypi.sh

--- scripts/install_testpypi.sh
+++ scripts/install_testpypi.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-VENV_PATH='~/Downloads'
-VENV_DIR='p3.6_env'
+
+VENV_PATH=$(echo $HOME)/Downloads
+VENV_DIR='p3.6_venv'
 PACKAGE='stsAval'
 
 
@@ -10,14 +11,18 @@ function venv(){
     #
     if [ ! $VIRTUAL_ENV ]; then
         if [ ! -d $VENV_PATH/$VENV_DIR ]; then
-            python3 -m venv $VENV_PATH/$VENV_DIR
+            echo -e "\nchanging to $VENV_PATH..."
+            cd $VENV_PATH
+            python3 -m venv $VENV_DIR
         fi
+        echo -e "\nSourcing venv $VENV_DIR..."
         source $VENV_PATH/$VENV_DIR/bin/activate
     fi
 }
-
 # source venv
 venv
-
+exit 0
 # install from test site
 pip install -U awscli --extra-index-url https://test.pypi.org/simple/ $PACKAGE 
+
+
