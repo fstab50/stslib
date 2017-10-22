ffe2218a43158b252510f267a1c1ceeee00e4e97
Blake Huber <blakeca00@gmail.com>
2017-10-22 08:32:41 -0500

    build_pypi.sh: script added for uploading new versions to pypi

---
+++ scripts/build_pypi.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env/bash
+
+PACKAGE='stsAval'
+root=$(git rev-parse --show-toplevel 2>/dev/null)
+HOME=$(echo $HOME)
+
+
+function update_minor_version(){
+        version=$(grep '__version__' $PACKAGE/_version.py  | head -n 1 | awk -F"'" '{print $2}')
+
+        if [ -z $(echo $version | awk -F '.' '{print $4}') ]; then
+            add='1'
+        else
+            add=$(bc -l <<< "$(echo $version | awk -F '.' '{print $4}') + 1")
+        fi
+        updated_version="$(echo $version | awk -F '.' '{print $1"."$2"."$3}').$add"
+        echo -e "\nupdated_version number is: $updated_version\n"
+        echo "__version__ = '${updated_version}'" > $root/stsAval/_version.py
+}
+
+
+# --- main --------------------------------------------------------------------
+
+
+if [ $root ]; then
+    cd $root     # git repo root dir
+else
+    echo -e '\nrepo root not found. Exit'
+    exit 1
+fi
+
+# convert readme to required
+pandoc --from=markdown --to=rst --output=README.rst README.md
+
+# build source dist
+python3 setup.py sdist
+
+# build python3-spec wheel
+python3 setup.py bdist_wheel
+
+# upload
+twine upload --repository pypi dist/*
+
+exit 0
