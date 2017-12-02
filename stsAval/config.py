13d2c549795872804f0f82da13744d8fc5816c1b
blakeca00 <blakeca00@gmail.com>
2017-12-02 12:16:26 -0600

    config.py: rebalance code beteen statics and module

--- stsAval/config.py
+++ stsAval/config.py
@@ -8,32 +8,49 @@ Module Attributes:
         Name of local config file, usually found in ~/.stsaval dir
     - logger (TYPE logging obj):
         system logger, output set by log_mode project-level attribute
-    - seed_file (TYPE str):
+    - config_seed (TYPE str):
         yaml config file template used to seed local config file if none exists
 """
 import os
 import yaml
 import logging
+from stsAval.statics import global_config['config_file']
 from stsAval._version import __version__
+
 
 logger = logging.getLogger(__version__)
 logger.setLevel(logging.INFO)
 
-config_file = 'config.yml'
 
 
-class LocalConfig():
+class UpdateConfig():
+    def __init__(self, local_file=''):
+        if os.path.exists(local_file):
+            self.update(local_file)
+        else:
+            logger.info(
+                'local config file [%s] not found, creating.' % config_file)
+            self.create(local_file)
+        return
+
+    def update(self):
+        """ updates values in local config file """
+
+    def create(cfg):
+        """ create new config file """
+        with open(cfg, 'w') as f1:
+            f1.write(config_seed)
+            f1.close()
+        return
+
+
+class ReadConfig():
     def __init__(self, local_file=''):
         if os.path.exists(local_file):
             self.read(local_file)
         else:
-            logger.info(
-                'local config file [%s] not found, creating.' %
-                global_config['config_file']
-                )
-            self.create(local_file)
-            self.read(local_file)
-        return
+            logger.info('local config file [%s] not found' % config_file)
+            return {}
 
     def read(self, cfg):
         """ reads values from local config file """
@@ -51,40 +68,6 @@ class LocalConfig():
             except yaml.YAMLError as exc:
                 print(exc)
 
-    def update(self):
-        """ updates values in local config file """
-
-    def create(cfg):
-        """ create new config file """
-        with open(cfg, 'w') as f1:
-            f1.write(seed_file)
-            f1.close()
-        return
-
-
-def read_local_config(cfg):
-    """ Parses local config file for override values
-
-    Args:
-        local_file (str):  filename of local config file
-
-    Returns:
-        dict of values contained in local config file
-    """
-    with open(cfg, 'r') as stream:
-        try:
-            yml_object = yaml.load(stream)
-            #print(yml_object)
-            return {
-                'log_mode': yml_object['LocalConfiguration']['logging']['log_mode'],
-                'log_file': yml_object['LocalConfiguration']['logging']['log_dir'] + yml_object['LocalConfiguration']['logging']['log_file'],
-                'debug': yml_object['LocalConfiguration']['debug'],
-                'profile_user': yml_object['LocalConfiguration']['profile_user'][0]['Default'],
-                'credential_format': yml_object['LocalConfiguration']['CredentialFormat'][0]['Default'],
-                }
-        except yaml.YAMLError as exc:
-            print(exc)
-
 
 if __name__ == '__main__':
-    LocalConfig(local_file=config_file)
+    UpdateConfig(local_file=global_config['config_file'])
